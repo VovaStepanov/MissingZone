b9fcba7ce40d3e65112f02b33942643f7a6d991d
VovaStepanov <vovastepanov741@gmail.com>
2024-05-23 18:12:01 +0300

    fix: fix api connection

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://missingzoneapi20240511172347.azurewebsites.net/",
+    baseURL: "https://missingzoneapi20240523175701.azurewebsites.net/",
 });
 
 api.interceptors.request.use(
